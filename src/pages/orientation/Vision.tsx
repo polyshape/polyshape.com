d158215cd52d7bc4fdb5f5e9ccecf9e8d32bf224
papamarkou <theodore.papamarkou@gmail.com>
2025-09-05 00:01:24 +0300

    Minor adjustment in a sentence under vision

--- src/pages/orientation/Vision.tsx
+++ src/pages/orientation/Vision.tsx
@@ -5,7 +5,7 @@ export default function Vision() {
     <div className="prose">
       <h1 className="hero__title">{AppRoutes.VISION.title}</h1>
       <p>
-        PolyShape envisions learning systems for healthcare built on mathematical foundations. These systems are modular and can be composed into larger systems. They reason under uncertainty, integrate diverse biomedical data and their interrelations, and support decisions that affect human health. Among their capabilities is the design of multi-agent learning systems that advance health intelligence.
+        PolyShape envisions learning systems for healthcare built on mathematical foundations. These systems are modular and can be composed into larger systems. They reason under uncertainty, integrate diverse biomedical data and their interrelations, and support decisions that affect human health. Among their capabilities is multi-agent learning that advances health intelligence.
       </p>
     </div>
   );

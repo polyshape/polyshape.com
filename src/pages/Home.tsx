13112597f44ff22fb9f9d12eaee82d7d32fde508
Marios Papamarkou <mariosdrth@yahoo.gr>
2025-09-10 08:17:45 +0200

    Fix images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,20 @@
 import { useNavigate } from 'react-router-dom';
 import { AppRoutes } from '../lib/common/AppRoutes';
+import { useTheme } from '../lib/common/ui/theme/useTheme';
 
 export default function Home() {
+  const { theme } = useTheme();
   const navigate = useNavigate();
-
   const goToVision = () => navigate(AppRoutes.VISION.path);
   const goToContact = () => navigate(AppRoutes.CONTACT.path);
+  const srcImgWebp = theme === "dark" ? "/home_hero_transparent_dark_mode.webp" : "/home_hero_transparent_light_mode.webp";
+  const srcImgPng = theme === "dark" ? "/home_hero_transparent_dark_mode.png" : "/home_hero_transparent_light_mode.png";
 
   return (
     <div className="hero">
       <picture className="hero__bg">
-        <source srcSet="/home_hero_transparent.webp" type="image/webp" />
-        <img src="/home_hero_transparent.png" alt="Background" />
+        <source srcSet={srcImgWebp} type="image/webp" />
+        <img src={srcImgPng} alt="Background" />
       </picture>
       <div className="hero__overlay" />
       <div className="hero__content">
